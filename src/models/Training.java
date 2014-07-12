081a3d6ce2ed376f5e5ba3579763a19c2070c255
MaysaMacedo <maysa.souza@ccc.ufcg.edu.br>
2014-07-12 19:57:35 -0300

    Adicionando o Trainig

---
+++ src/models/Training.java
@@ -0,0 +1,36 @@
+package models;
+
+import java.util.List;
+
+public class Training {
+	
+	private StopButton stopButton;
+	private List<Integer> rank;
+	
+	public Training (StopButton stopButton) {
+		this.stopButton = stopButton;
+	}
+	
+	public void addScore(int score) {
+		rank.add(score);
+	}
+
+	public List<Integer> getRank() {
+		return rank;
+	}
+
+	public void setRank(List<Integer> rank) {
+		this.rank = rank;
+	}
+
+	public StopButton getStopButton() {
+		return stopButton;
+	}
+
+	public void setStopButton(StopButton stopButton) {
+		this.stopButton = stopButton;
+	}
+	
+	
+
+}
