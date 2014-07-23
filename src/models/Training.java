678305296e932b13fbe02db562f4f32db4fde862
MaysaMacedo <maysa.souza@ccc.ufcg.edu.br>
2014-07-22 23:32:15 -0300

    Documenting

--- src/models/Training.java
+++ src/models/Training.java
@@ -2,31 +2,63 @@ package models;
 
 import java.util.List;
 
+/**
+ * Training class 
+ * @author Ana, Felipe, Karen, Leticia, Marcos and Maysa.
+ *
+ */
 public class Training {
 	
 	private StopButton stopButton;
-	private List<Match> rank;
+	private List<Round> rank;
 	
+	/**
+	 * Creates a section of training with a button
+	 * @param stopButton
+	 */
 	public Training (StopButton stopButton) {
 		this.stopButton = stopButton;
 	}
 	
-	public void addMatch(Match match) {
-		rank.add(match);
+	/**
+	 * Add the round to the rank
+	 * @param round
+	 * 		The round that was completed
+	 */
+	public void addRound(Round round) {
+		rank.add(round);
 	}
 
-	public List<Match> getRank() {
+	/**
+	 * Returns the rank
+	 * @return the existing rank 
+	 */
+	public List<Round> getRank() {
 		return rank;
 	}
 
-	public void setRank(List<Match> rank) {
+	/**
+	 * Change the actual Rank 
+	 * @param rank
+	 * 		The new rank 
+	 */
+	public void setRank(List<Round> rank) {
 		this.rank = rank;
 	}
 
+	/**
+	 * Returns the stop Button 
+	 * @return the stop button
+	 */
 	public StopButton getStopButton() {
 		return stopButton;
 	}
 
+	/**
+	 * Change the actual stop Button
+	 * @param stopButton
+	 * 		The new stop button
+	 */
 	public void setStopButton(StopButton stopButton) {
 		this.stopButton = stopButton;
 	}
